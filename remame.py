cdf61d7ed3f116dbd66171200024b872ce7e79af
jm-mirza <60502844+jm-mirza@users.noreply.github.com>
2020-02-03 14:40:22 +0100

    Add files via upload

---
+++ remame.py
@@ -0,0 +1,24 @@
+import os
+# Function to rename multiple files
+def main():
+
+
+   i = 0
+###Add path to your file###
+   path="path/to/your/file"
+
+   for filename in os.listdir(path):
+
+###Add phrase with which you want your names to be appended###
+
+      my_dest ="ADD WORD/PHRASE HERE" + str(i) + ".txt"
+      my_source =path + filename
+      my_dest =path + my_dest
+      # rename() function will
+      # rename all the files
+      os.rename(my_source, my_dest)
+      i += 1
+
+# Driver Code
+if __name__ == '__main__':
+	main()
